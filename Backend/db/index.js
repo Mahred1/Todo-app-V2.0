7d1bfb3d43536536e99a0321bb10bcb85e80c6aa
Mahred1 <mredwan.kemal@gmail.com>
2025-04-27 09:27:54 +0300

    created basic schemas and first post request

---
+++ Backend/db/index.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb+srv://markreonal815:5cSEjIrVDPj6egP5@cluster0.qudzmnh.mongodb.net/Todo")
+
+const todoSchema = mongoose.Schema({
+    title:String,
+    description:String,
+    isDone:Boolean
+})
+
+const Todos = mongoose.model('Todos',todoSchema)
+
+
---
+++ Backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+
+const app = express()
+
+app.post('/todos',(req,res)=>{
+    const createPayload = req.body
+
+    Todo
+})
+
+app.listen(3000)
